b71326fb49efa8a0e2631db2963ed5eb554432f1
Rafaela Simões <rafaelasimoes2203@gmail.com>
2023-09-08 16:12:44 -0300

    pokemon escolhido batalhando

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,19 @@ class Pokemon {
   Pokemon({required this.name, required this.hp, required this.attack});
 }
 
-class PokemonChoice {
-  final String name;
-  final int id;
-
-  PokemonChoice({required this.name, required this.id});
-}
-
 class PokemonBattleScreen extends StatefulWidget {
   @override
   _PokemonBattleScreenState createState() => _PokemonBattleScreenState();
 }
 
 class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
+  List<Pokemon> availablePokemons = [
+    Pokemon(name: 'Charizard', hp: 100, attack: 50),
+    Pokemon(name: 'Blastoise', hp: 120, attack: 45),
+    Pokemon(name: 'Venusaur', hp: 110, attack: 48),
+  ];
+
   Pokemon? playerPokemon;
-  List<PokemonChoice>? playerPokemonChoices;
-  PokemonChoice? selectedPlayerPokemonChoice;
   Pokemon? opponentPokemon;
   bool isPlayerTurn = true;
   bool isBattleOver = false;
@@ -45,14 +42,6 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
   @override
   void initState() {
     super.initState();
-    // Initialize player's Pokémon choices for the first generation (1 to 151)
-    getRandomPokemonChoices(1).then((choices) {
-      setState(() {
-        playerPokemonChoices = choices;
-      });
-    });
-
-    // Initialize the opponent's Pokémon randomly
     getRandomPokemon().then((pokemon) {
       setState(() {
         opponentPokemon = pokemon;
@@ -60,34 +49,9 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
     });
   }
 
-  Future<List<PokemonChoice>> getRandomPokemonChoices(int generation) async {
-    final List<PokemonChoice> choices = [];
-    final random = Random();
-
-    for (int i = 0; i < 3; i++) {
-      final int randomPokemonId = random.nextInt(151) + 1; // Limitado à primeira geração (1 a 151)
-      final response = await http.get(
-        Uri.parse('https://pokeapi.co/api/v2/pokemon/$randomPokemonId/'),
-      );
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        final name = data['name'];
-        final id = data['id'];
-
-        final choice = PokemonChoice(name: name, id: id);
-        choices.add(choice);
-      } else {
-        throw Exception('Falha ao carregar dados do Pokémon');
-      }
-    }
-
-    return choices;
-  }
-
   Future<Pokemon> getRandomPokemon() async {
     final random = Random();
-    final int randomPokemonId = random.nextInt(807) + 1; // PokeAPI tem até o Pokémon #807
+    final int randomPokemonId = random.nextInt(807) + 1;
 
     final response = await http.get(
       Uri.parse('https://pokeapi.co/api/v2/pokemon/$randomPokemonId/'),
@@ -96,8 +60,8 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final name = data['name'];
-      final hp = data['stats'][5]['base_stat']; // HP está no índice 5 da lista de stats
-      final attack = data['stats'][4]['base_stat']; // Ataque está no índice 4 da lista de stats
+      final hp = data['stats'][5]['base_stat'];
+      final attack = data['stats'][4]['base_stat'];
 
       return Pokemon(name: name, hp: hp, attack: attack);
     } else {
@@ -105,24 +69,26 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
     }
   }
 
+  void choosePlayerPokemon(Pokemon selectedPokemon) {
+    setState(() {
+      playerPokemon = selectedPokemon;
+    });
+  }
+
   void attack() {
     if (!isBattleOver) {
       if (isPlayerTurn) {
-        // Player attacks
         final damage = playerPokemon!.attack;
         opponentPokemon!.hp -= damage;
       } else {
-        // Opponent attacks
         final damage = opponentPokemon!.attack;
         playerPokemon!.hp -= damage;
       }
 
-      // Check if the battle is over
       if (playerPokemon!.hp <= 0 || opponentPokemon!.hp <= 0) {
         isBattleOver = true;
         showResultDialog();
       } else {
-        // Switch turns
         isPlayerTurn = !isPlayerTurn;
       }
     }
@@ -158,34 +124,15 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
   }
 
   void restartBattle() {
-    playerPokemonChoices = null;
-    selectedPlayerPokemonChoice = null;
-    getRandomPokemonChoices(1).then((choices) {
-      setState(() {
-        playerPokemonChoices = choices;
-      });
-    });
-
+    playerPokemon = null;
     getRandomPokemon().then((pokemon) {
       setState(() {
         opponentPokemon = pokemon;
       });
     });
-
     isPlayerTurn = true;
     isBattleOver = false;
     setState(() {});
-  }
-
-  void selectPlayerPokemon(PokemonChoice choice) {
-    final int selectedPokemonId = choice.id;
-    getRandomPokemon().then((pokemon) {
-      setState(() {
-        playerPokemon = pokemon;
-        selectedPlayerPokemonChoice = choice;
-        playerPokemonChoices = null;
-      });
-    });
   }
 
   @override
@@ -220,25 +167,22 @@ class _PokemonBattleScreenState extends State<PokemonBattleScreen> {
                       ),
                     ],
                   )
-                : playerPokemonChoices != null
-                    ? Column(
-                        children: [
-                          Text('Escolha seu Pokémon Inicial:'),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              for (var choice in playerPokemonChoices!)
-                                ElevatedButton(
-                                  onPressed: () {
-                                    selectPlayerPokemon(choice);
-                                  },
-                                  child: Text(choice.name),
-                                ),
-                            ],
-                          ),
-                        ],
-                      )
-                    : SizedBox(),
+                : Column(
+                    children: [
+                      Text('Escolha seu Pokémon:'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: availablePokemons
+                            .map(
+                              (pokemon) => ElevatedButton(
+                                onPressed: () => choosePlayerPokemon(pokemon),
+                                child: Text(pokemon.name),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ],
+                  ),
           ],
         ),
       ),
